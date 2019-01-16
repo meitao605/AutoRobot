b980bf1400b6a342396c662d2d9767b3dfe6cbf7
meitao605 <tao.mei@lifesig.com>
2019-01-17 00:33:11 +0800

    first commit

---
+++ AutoRobot/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace AutoRobot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Start = new System.Windows.Forms.Button();
+            this.Stop = new System.Windows.Forms.Button();
+            this.ReconnectCamera = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // Start
+            // 
+            this.Start.Location = new System.Drawing.Point(640, 25);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(116, 54);
+            this.Start.TabIndex = 0;
+            this.Start.Text = "Start";
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // Stop
+            // 
+            this.Stop.Location = new System.Drawing.Point(640, 111);
+            this.Stop.Name = "Stop";
+            this.Stop.Size = new System.Drawing.Size(116, 54);
+            this.Stop.TabIndex = 0;
+            this.Stop.Text = "Stop";
+            this.Stop.UseVisualStyleBackColor = true;
+            this.Stop.Click += new System.EventHandler(this.Stop_Click);
+            // 
+            // ReconnectCamera
+            // 
+            this.ReconnectCamera.Location = new System.Drawing.Point(218, 12);
+            this.ReconnectCamera.Name = "ReconnectCamera";
+            this.ReconnectCamera.Size = new System.Drawing.Size(116, 35);
+            this.ReconnectCamera.TabIndex = 0;
+            this.ReconnectCamera.Text = "Connect Camera";
+            this.ReconnectCamera.UseVisualStyleBackColor = true;
+            this.ReconnectCamera.Click += new System.EventHandler(this.ReconnectCamera_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("宋体", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(22, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(181, 35);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.ReconnectCamera);
+            this.Controls.Add(this.Stop);
+            this.Controls.Add(this.Start);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.Button Stop;
+        private System.Windows.Forms.Button ReconnectCamera;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ AutoRobot/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Jai_FactoryDotNET;
+
+namespace AutoRobot
+{
+    public partial class Form1 : Form
+    {
+        CFactory myFactory = new CFactory();
+        CCamera myCamera;
+
+        private string CameraName;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Jai_FactoryWrapper.EFactoryError error = Jai_FactoryWrapper.EFactoryError.Success;
+            error = myFactory.Open("");
+            InitCamera();
+            StartCamera();
+        }
+
+        private void StartCamera()
+        {
+            if (myCamera != null)
+                myCamera.StartImageAcquisition(false, 5);
+        }
+
+        private void InitCamera()
+        {
+            if(myCamera!=null)
+            {
+                if (myCamera.IsOpen)
+                {
+                    myCamera.Close();
+                }
+                myCamera = null;   
+            }
+
+            myFactory.UpdateCameraList(Jai_FactoryDotNET.CFactory.EDriverType.FilterDriver);
+
+            for (int i = 0; i < myFactory.CameraList.Count; i++)
+            {
+                myCamera = myFactory.CameraList[i];
+                if(Jai_FactoryWrapper.EFactoryError.Success==myCamera.Open())
+                {
+                    break;
+                }
+            }
+
+            if (null != myCamera && myCamera.IsOpen)
+            {
+                CameraName = myCamera.CameraID;
+                CameraName = CameraName.Substring(CameraName.Length - 10);
+                comboBox1.Items.Add(CameraName);
+                comboBox1.SelectedIndex = 0;
+                // Attach an event that will be called every time the Async Recording finishes
+                myCamera.AsyncImageRecordingDoneEvent += new CCamera.AsyncImageRecordingDoneHandler(myCamera_AsyncImageRecordingDoneEvent);
+
+                if (myCamera.NumOfDataStreams > 0)
+                {
+                    Start.Enabled = true;
+                    Stop.Enabled = true;
+                }
+                else
+                {
+                    Start.Enabled = false;
+                    Stop.Enabled = false;
+                } }
+            else
+            {
+                Start.Enabled = false;
+                Stop.Enabled = false;
+                MessageBox.Show("No Cameras Found!");
+            }
+        }
+
+        private void Start_Click(object sender, EventArgs e)
+        {
+            StartCapture();
+        }
+
+        private void Stop_Click(object sender, EventArgs e)
+        {
+            if (myCamera != null)
+                myCamera.StopImageAcquisition();
+        }
+
+        private void StartCapture()
+        {
+            if (myCamera != null)
+            {
+                if (myCamera.IsAsyncImageRecordingRunning || (myCamera.TotalAsyncImagesRecordedCount > 0))
+                {
+
+                        myCamera.StopAsyncImageRecording();
+                        myCamera.FreeAsyncRecordedImages();
+                        myCamera.StartAsyncImageRecording(1, 0, 0);        
+                }
+                else
+                {
+                    myCamera.StartAsyncImageRecording(1, 0, 0);
+                }
+            }
+        }
+
+
+        private void SaveCamera()
+        {
+            // Have we got any images to save to disk?
+            if (myCamera != null && !myCamera.IsAsyncImageRecordingRunning && (myCamera.TotalAsyncImagesRecordedCount > 0))
+            {
+                    // Disable the Image Recording buttons as long as we are saving the images
+
+                    // Get the recorded images as a list
+                    List<Jai_FactoryWrapper.ImageInfo> imageList = myCamera.GetAsyncRecordedImages();
+
+                    // Any images recorded?
+                    if (imageList != null && (imageList.Count > 0))
+                    {
+                        // Run through the list of recorded images
+                        for (int index = 0; index < myCamera.TotalAsyncImagesRecordedCount; index++)
+                        {
+                            Jai_FactoryWrapper.EFactoryError error = Jai_FactoryWrapper.EFactoryError.Success;
+
+                            // Get the recorded image at this index
+                            Jai_FactoryWrapper.ImageInfo ii = imageList[index];
+
+                            // Are we saving the images in "raw" format or in Tiff?
+
+          
+                                // Create local image that will contain the converted image
+                                Jai_FactoryWrapper.ImageInfo localImageInfo = new Jai_FactoryWrapper.ImageInfo();
+
+                                // Allocate buffer that will contain the converted image
+                                // In this sample we re-allocate the buffer over-and-over because we assume that the recorded images could be
+                                // of different size (If we have been using the Sequence functionality in the cameras)
+                                error = Jai_FactoryWrapper.J_Image_Malloc(ref ii, ref localImageInfo);
+
+                                // Convert the raw image to image format
+                                error = Jai_FactoryWrapper.J_Image_FromRawToImage(ref ii, ref localImageInfo, 4096, 4096, 4096);
+
+                        // Save the image to disk
+                        string time = System.DateTime.Now.Hour.ToString()+"-"+ System.DateTime.Now.Minute.ToString()+"-"+ System.DateTime.Now.Second.ToString();
+                                error = Jai_FactoryWrapper.J_Image_SaveFile(ref localImageInfo, ".\\RecordedImage" + time + ".tif");
+
+                                //Free the conversion buffer
+                                error = Jai_FactoryWrapper.J_Image_Free(ref localImageInfo);
+
+                            Application.DoEvents();
+                        }
+
+                    }              
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (myCamera != null)
+            {
+                myCamera.StopImageAcquisition();
+                myCamera.Close();
+            }
+        }
+        void myCamera_AsyncImageRecordingDoneEvent(int Count)
+        {
+            SaveCamera();       
+        }
+
+        private void ReconnectCamera_Click(object sender, EventArgs e)
+        {
+            InitCamera();
+        }
+    }
+}
